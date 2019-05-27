250336d6984e0005aaf893c8b32be9b28965d764
Igor <dubkovigor1488@gmail.com>
2019-05-27 22:00:08 +0300

    2commit

---
+++ frontend/src/app/modules/layout/components/main/register/register.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from '../../../../models/user';
+import {HttpService} from '../../../../../services/http.service';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
+import {ModalService} from '../../../../../services/ModalService';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  user: User = new User();
+  repeatPassword: string;
+  constructor(private httpService: HttpService, private router: Router, private modalService: ModalService, private toastr: ToastrService) { }
+
+  saveUser() {
+    if (this.user.password === this.repeatPassword  ) {
+      console.log(this.user);
+      this.httpService.saveUser(this.user)
+        .subscribe((data) => {
+          console.log(data);
+          this.router.navigateByUrl('/main');
+          this.toastr.success('Вы зарегистрированы!');
+          this.modalService.closeModal();
+        });
+    } else {
+      this.toastr.info('Проверьте данные еще раз');
+    }
+  }
+
+  ngOnInit() {
+
+  }
+
+}
